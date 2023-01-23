815128a9ab18810ad31dd73f29b33d0e11243731
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-24 02:04:03 +0300

    Завершение задачи №4.

---
+++ Ex_4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine()!);
+
+double numberA = number % 10;
+
+if (numberA % 2 == 0)
+{
+    double numberB =  Math.Pow(number, 3);
+    Console.WriteLine("Введенное число в кубе: " + numberB);
+}
+else
+{
+    Console.WriteLine("Введенное число не заканчивается на четную цифру!");
+}
