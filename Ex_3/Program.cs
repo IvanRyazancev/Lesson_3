408e22630e61e16211291dd12842439dcdc04a71
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-24 01:35:23 +0300

    Завершение задачи №3.

---
+++ Ex_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число N : ");
+int N = int.Parse(Console.ReadLine()!);
+
+int multiplic = 1;
+
+for (int count = 1; count <= N; count++)
+{
+    multiplic = multiplic * count;    
+}
+
+Console.WriteLine("Произведение чисел от 1 до " + N + " = " + multiplic);
