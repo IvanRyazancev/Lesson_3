8277e0c3c686817a9b42e02f318a7dc0a7f6d752
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-23 21:23:52 +0300

    Окончание задачи №1.

---
+++ Ex_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача №1. Найти кубы чисел от 1 до N
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введенное число в кубе: " + Math.Pow(number, 3));
