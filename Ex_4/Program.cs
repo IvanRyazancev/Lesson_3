14c0bc50c7335f866846d28c964ad00ebadbceff
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-24 14:43:21 +0300

    Корректировка и завершение задачи №4. Приведение решения в соответствие с условиями задачи.

--- Ex_4/Program.cs
+++ Ex_4/Program.cs
@@ -1,16 +1,25 @@
 ﻿//Показать кубы чисел, заканчивающихся на четную цифру
 
-Console.Write("Введите число: ");
+Console.Write("Введите конечное число N: ");
 double number = Convert.ToDouble(Console.ReadLine()!);
 
 double numberA = number % 10;
-
+        
 if (numberA % 2 == 0)
 {
-    double numberB =  Math.Pow(number, 3);
-    Console.WriteLine("Введенное число в кубе: " + numberB);
+    for (int i = 2; i <= number; i += 2)
+    {
+        double numberB =  Math.Pow(i, 3);
+        Console.WriteLine("Число последовательности от 1 до " + number + " : " + i);                    
+        Console.WriteLine("Число " + i + " в кубе: " + numberB);
+    }
 }
 else
 {
-    Console.WriteLine("Введенное число не заканчивается на четную цифру!");
+    for (int i = 0; i <= number; i += 2)
+    {
+        double numberB =  Math.Pow(i, 3);
+        Console.WriteLine("Число последовательности от 1 до " + number + " : " + i);                    
+        Console.WriteLine("Число " + i + " в кубе: " + numberB);
+    }    
 }
